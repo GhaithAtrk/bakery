60ad13e4c4f3e37d7f2cedf885c0342b7384422a
Ghaith <ghaithatrk93@gmail.com>
2024-04-06 13:55:15 +0800

    fixing header on mob view

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,11 +29,11 @@ const Header = () => {
   return (
     <div className="fixed z-10">
       <div className="relative w-screen">
+        {/* web view */}
         <div
-          className={`w-[90%]  rounded-full mx-auto mt-2 border-black border-b-4 p-2 md:px-8 md:py-4 ${colors.primary}`}
+          className={`hidden md:block w-[90%] rounded-full mx-auto mt-2 border-black border-b-4 p-2 md:px-8 md:py-4 ${colors.primary}`}
         >
-          {/* web view */}
-          <div className="hidden md:flex justify-between">
+          <div className="flex justify-between">
             <nav>
               <ul className="md:flex gap-4 text-xl">
                 <li className="hover:text-white cursor-pointer">
@@ -74,61 +74,63 @@ const Header = () => {
               </ul>
             </nav>
           </div>
-          {/* mobile view */}
-          <div className="flex justify-between items-center md:hidden">
-            <Link href="/">
+        </div>
+        {/* mobile view */}
+        <div
+          className={`md:hidden w-[90%] h-[50px] rounded-full mx-auto mt-2 border-black border-b-4 p-2 md:px-8 md:py-4 ${colors.primary}`}
+        >
+          <Link href="/">
+            <div
+              className={`absolute top-2 left-6  w-12 h-12  flex flex-col items-center bg-white  rounded-full border-2 border-black `}
+            >
+              <Image src="/bread.png" alt="" width={150} height={150} />
               <div
-                className={` w-12 h-12  flex flex-col items-center bg-white  rounded-full border-2 border-black `}
+                className={`${dancingScript.className} w-[70px] text-xs bg-orange-300 border-2 flex justify-center border-black`}
               >
-                <Image src="/bread.png" alt="" width={150} height={150} />
-                <div
-                  className={`${dancingScript.className} w-16 text-xs bg-orange-300 border-2 flex justify-center border-black`}
-                >
-                  Bread Bakery
-                </div>
+                Bread Bakery
               </div>
-            </Link>
-            <div className="relative">
-              <button
-                type="button"
-                className="rounded-full bg-white border-2 border-black p-3 "
-                id="user-menu-button"
-                aria-expanded="false"
-                aria-haspopup="true"
-                onClick={() => setToggleMenu(!toggleMenu)}
-              >
-                <span className="text-white ">
-                  {toggleMenu ? (
-                    <Image src="/menu.png" alt="" width={10} height={10} />
-                  ) : (
-                    <Image src="/close.png" alt="" width={10} height={10} />
-                  )}
-                </span>
-              </button>
+            </div>
+          </Link>
+          <div className="absolute top-1 right-6">
+            <button
+              type="button"
+              className="rounded-full bg-white border-2 border-black p-3 "
+              id="user-menu-button"
+              aria-expanded="false"
+              aria-haspopup="true"
+              onClick={() => setToggleMenu(!toggleMenu)}
+            >
+              <span className="text-white ">
+                {toggleMenu ? (
+                  <Image src="/menu.png" alt="" width={10} height={10} />
+                ) : (
+                  <Image src="/close.png" alt="" width={10} height={10} />
+                )}
+              </span>
+            </button>
 
-              <div
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-                tabIndex="-1"
-                hidden={toggleMenu}
-              >
-                <ul className="text-center text-xl">
-                  <li className="hover:text-orange-500 cursor-pointer py-2">
-                    <Link href="#products">Products</Link>
-                  </li>
-                  <li className="hover:text-orange-500 cursor-pointer py-2">
-                    <Link href="#deal">Deals</Link>
-                  </li>
-                  <li className="hover:text-orange-500 cursor-pointer py-2">
-                    <Link href="#about">About Us</Link>
-                  </li>
-                  <li className="hover:text-orange-500 cursor-pointer py-2">
-                    <Link href="#testimonies">Testimonies</Link>
-                  </li>
-                </ul>
-              </div>
+            <div
+              className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="user-menu-button"
+              tabIndex="-1"
+              hidden={toggleMenu}
+            >
+              <ul className="text-center text-xl">
+                <li className="hover:text-orange-500 cursor-pointer py-2">
+                  <Link href="#products">Products</Link>
+                </li>
+                <li className="hover:text-orange-500 cursor-pointer py-2">
+                  <Link href="#deal">Deals</Link>
+                </li>
+                <li className="hover:text-orange-500 cursor-pointer py-2">
+                  <Link href="#about">About Us</Link>
+                </li>
+                <li className="hover:text-orange-500 cursor-pointer py-2">
+                  <Link href="#testimonies">Testimonies</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
